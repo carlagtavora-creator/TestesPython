527710df7b8f2e9b709807ca4ea6b296587774eb
carlagtavora-creator <carlag.tavora@gmail.com>
2026-07-22 17:34:07 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -52,6 +52,26 @@ while True: #condição de parada do loop
             resto = int(lista_valor[0]) % int(lista_valor[1])
             print(f"O resultado da divisão é: {divisao} e o resto é: {resto}")
 
+    if opcao == "5":
+        print("Potência")
+        num1 = str(input("Digite o número base e expoente: "))
+        lista_valor = list(num1.split(" "))
+        potencia = int(lista_valor[0]) ** int(lista_valor[1])
+        print(f"O resultado da potência é: {potencia}")
+    
+    if opcao == "6":
+        print("Raiz Quadrada")
+        num1 = str(input("Digite o número: "))
+        lista_valor = list(num1.split(" "))
+        lista_raizes = [] #cria lista vazia para armazenar os resultados das raízes quadradas
+        for num1 in lista_valor: #integração dos novos numeros na lista
+            lista_raizes.append(float(num1) ** (0.5)) #calcula a raiz quadrada e adiciona à lista   
+        for i in range(len(lista_valor)): #integração dos resultados na lista
+            print(f"O resultado da raiz quadrada é: {lista_raizes[i]}")
+
+
+
+
     if opcao == "9":
         print("Ajuda")
         print("Para utilizar a calculadora, selecione uma das opções do menu e siga as instruções.")
